630e991d4bf2a219bb50fc272e3ead2cd2085f39
sangwookkhu <ksw090711@gmail.com>
2025-10-27 13:07:19 +0900

    feat: Racing Controller 클래스 구현, Application 메인 클래스 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,4 +1,46 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.model.Cars;
+import racingcar.model.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    
+    public RacingController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+    
+    public void run() {
+        try {
+            String carNames = inputView.readCarNames();
+            int tries = inputView.readTries();
+            
+            Cars cars = new Cars(carNames);
+            RacingGame racingGame = new RacingGame(cars, tries);
+            
+            racingGame.run();
+            
+            outputView.printResultHeader();
+            printRaceResults(racingGame);
+            
+            List<String> winners = racingGame.getWinners();
+            outputView.printWinners(winners);
+            
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+    
+    private void printRaceResults(RacingGame racingGame) {
+        List<List<racingcar.model.Car>> raceHistory = racingGame.getRaceHistory();
+        for (List<racingcar.model.Car> cars : raceHistory) {
+            outputView.printRoundResult(cars);
+        }
+    }
 }
