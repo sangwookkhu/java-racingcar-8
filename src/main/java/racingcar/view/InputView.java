4cd15b77049b3efb7d9d03ba398d73e2c7f89d1d
sangwookkhu <ksw090711@gmail.com>
2025-10-27 12:44:49 +0900

    feat: InputView 클래스 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,25 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIES_PROMPT = "시도할 횟수는 몇 회인가요?";
+    
+    public String readCarNames() {
+        System.out.println(CAR_NAMES_PROMPT);
+        return Console.readLine();
+    }
+    
+    public int readTries() {
+        System.out.println(TRIES_PROMPT);
+        String input = Console.readLine();
+        
+        try {
+            int tries = Integer.parseInt(input);
+            return tries;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
+    }
 }
