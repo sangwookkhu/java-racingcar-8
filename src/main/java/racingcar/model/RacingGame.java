c015288d551104543f86ded59a82bc7a3d861df8
sangwookkhu <ksw090711@gmail.com>
2025-10-27 12:17:19 +0900

    feat: RacingGame 클래스 경주 진행 기능 구현

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,4 +1,42 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    private static final int MIN_TRIES = 1;
+    
+    private final Cars cars;
+    private final int tries;
+    private final RandomNumberGenerator randomNumberGenerator;
+    private final List<List<Car>> raceHistory;
+    
+    public RacingGame(Cars cars, int tries) {
+        validateTries(tries);
+        this.cars = cars;
+        this.tries = tries;
+        this.randomNumberGenerator = new RandomNumberGenerator();
+        this.raceHistory = new ArrayList<>();
+    }
+    
+    private void validateTries(int tries) {
+        if (tries < MIN_TRIES) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
+    
+    public void run() {
+        for (int i = 0; i < tries; i++) {
+            cars.moveAll(randomNumberGenerator);
+            raceHistory.add(new ArrayList<>(cars.getCars()));
+        }
+    }
+    
+    public List<List<Car>> getRaceHistory() {
+        return raceHistory;
+    }
+    
+    public List<String> getWinners() {
+        return cars.getWinners();
+    }
 }
