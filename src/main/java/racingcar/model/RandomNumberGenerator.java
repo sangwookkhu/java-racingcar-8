54472784908f224d777ed562733e65593f59bdd5
sangwookkhu <ksw090711@gmail.com>
2025-10-27 12:33:47 +0900

    RandomNumberGenerator 클래스 구현

--- src/main/java/racingcar/model/RandomNumberGenerator.java
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -1,5 +1,10 @@
 package racingcar.model;
 
 public class RandomNumberGenerator {
-   
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+    
+    public int generate() {
+        return (int) (Math.random() * (MAX_VALUE - MIN_VALUE + 1)) + MIN_VALUE;
+    }
 }
