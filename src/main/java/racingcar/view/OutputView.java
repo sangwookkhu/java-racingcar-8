ecbbef17e150fa7ab231ab5262353b9ee71ce239
sangwookkhu <ksw090711@gmail.com>
2025-10-27 12:56:47 +0900

    feat: OutputView 클래스 출력 기눙 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,37 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_PREFIX = "최종 우승자 : ";
+    
+    public void printResultHeader() {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+    }
+    
+    public void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            printCarStatus(car);
+        }
+        System.out.println();
+    }
+    
+    private void printCarStatus(Car car) {
+        System.out.print(car.getName() + " : ");
+        printPosition(car.getPosition());
+        System.out.println();
+    }
+    
+    private void printPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+    }
+    
+    public void printWinners(List<String> winners) {
+        System.out.print(WINNER_PREFIX + String.join(", ", winners));
+    }
 }
